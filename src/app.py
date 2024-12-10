19daa4c8a9b3443ae18fa29a7da7c896954a3816
toyosi-powerlabs <toyosi@powerlabstech.com>
2024-12-10 15:22:39 +0100

    change

--- src/app.py
+++ src/app.py
@@ -196,4 +196,4 @@ def predict_water_level(input_data: UserInput):
 # Run the FastAPI server
 if __name__ == '__main__':
     import uvicorn
-    uvicorn.run(app, host="127.0.0.1", port=8001)
+    uvicorn.run(app, host="127.0.0.1", port=8002)
